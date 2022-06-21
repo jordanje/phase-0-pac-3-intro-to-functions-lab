5057c81b5f662732341c4884f27296cf3f45dfe5
Jordan Jennings <jordanjennings12@gmail.com>
2022-06-20 20:30:50 -0400

    commit tree 53f690e6c5290382e002b26efc05968a8d05345d

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(string){
+    return string.toUpperCase();
+}
+
+shout("hello");
+
+function whisper(string){
+    return string.toLowerCase();
+}
+    
+whisper("hello");
+
+function logShout(string){
+    console.log(string.toUpperCase())
+}
+
+logShout("hi");
